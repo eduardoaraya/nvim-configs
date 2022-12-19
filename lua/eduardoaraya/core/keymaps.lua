00881788d71daa38ff5c30897d446b6244e5e015
eduardoaraya <eduardojezine@gmail.com>
2022-12-19 19:37:27 -0300

    configurations

--- lua/eduardoaraya/core/keymaps.lua
+++ lua/eduardoaraya/core/keymaps.lua
@@ -2,8 +2,30 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+
+-- shortcuts
 keymap.set("i", "jk", "<ESC>")
+keymap.set("n", "<C-s>", ":w<CR>")
+
 
 
 -- plugins
+
+-- Tree explorer
 keymap.set("n", "<leader>b", ":NvimTreeToggle<CR>")
+
+-- split window
+keymap.set("n", "<leader>1", "<C-w>v")
+keymap.set("n", "<leader>2", "<C-w>s")
+keymap.set("n", "<leader>3", "<C-w>=")
+keymap.set("n", "<leader>x", ":close<CR>")	
+
+
+-- manager tabs
+keymap.set("n", "<leader>n", ":tabnew<CR>")	
+keymap.set("n", "<leader>c", ":tablose<CR>")	
+keymap.set("n", "<leader>x", ":close<CR>")
+keymap.set("n", "<leader>tn", ":tabn<CR>")
+keymap.set("n", "<leader>tp", ":tabp<CR>")	
+
+keymap.set("n", "<leader>tm", ":MaximizerToggle<CR>")
--- lua/eduardoaraya/plugins-setup.lua
+++ lua/eduardoaraya/plugins-setup.lua
@@ -1,17 +1,20 @@
 local cmd = vim.cmd
 
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    cmd [[packadd packer.nvim]]
-    return true
-  end
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+ 
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        cmd [[packadd packer.nvim]]
+        return true
+    end
   return false
 end
 
+
 local packer_bootstrap = ensure_packer()
+
 
 cmd([[
   augroup packer_user_config
@@ -20,22 +23,38 @@ cmd([[
   augroup end
 ]])
 
-local status, packer = pcall(require, "packer")
 
+local status, packer = pcall(require, "packer")
 
 if not status then
 	print("Status not found on: plugins-setup.lua")
-	return
+ return
 end
 
 
 return packer.startup(function(use)
-	
-	use("dracula/vim")
+	-- Plugin to load other plugins
+    use("wbthomason/packer.nvim")
+    use("nvim-lua/plenary.nvim")
+
+     
+	-- THEME 
+    use("dracula/vim")
+
+    -- UTILS
     use("christoomey/vim-tmux-navigator")
     use("nvim-tree/nvim-tree.lua")
     use("kyazdani42/nvim-web-devicons")
-    if (packer_bootstrap) then
+    use("szw/vim-maximizer")
+    
+    -- ESSENTIALS
+    use("tpope/vim-surround")
+    use("vim-scripts/ReplaceWithRegister")
+    use("numToStr/Comment.nvim")
+
+    
+    if (packer_bootstrap) then     
+        print("sync packers")
         require("packer").sync()
     end
 end)
