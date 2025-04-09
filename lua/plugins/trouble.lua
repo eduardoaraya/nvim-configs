1fb18f47243d95fb247249b92f354498312c212f
eduardoaraya <eduardojezine@gmail.com>
2025-04-09 13:00:42 -0300

    Setup diagnostic icons

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -9,13 +9,17 @@ return {
       position = "left",
       use_diagnostic_sign = true,
       icons = true,
-      signs = {
-        error = "✗",
-        warning = "",
-        hint = "",
-        information = ""
-      },
     })
+
+    vim.fn.sign_define("DiagnosticSignError",
+    {text = "✗ ", texthl = "DiagnosticSignError"})
+    vim.fn.sign_define("DiagnosticSignWarn",
+    {text = " ", texthl = "DiagnosticSignWarn"})
+    vim.fn.sign_define("DiagnosticSignInfo",
+    {text = " ", texthl = "DiagnosticSignInfo"})
+    vim.fn.sign_define("DiagnosticSignHint",
+    {text = "", texthl = "DiagnosticSignHint"})
+
     vim.keymap.set("n", "<leader>;", function() trouble.toggle() end)
     vim.keymap.set("n", "<leader>;w", function() trouble.toggle("workspace_diagnostics") end)
     vim.keymap.set("n", "<leader>;d", function() trouble.toggle("document_diagnostics") end)
